ff0940d8d98bac76d0fb4aaba00d21e8206b4db0
Maxim Paymushkin <bmx666@users.noreply.github.com>
2024-05-15 23:04:04 +0200

    build.gradle.kts: update dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -100,8 +100,8 @@ dependencies {
     implementation("androidx.compose.material3:material3-window-size-class")
 
     implementation("androidx.activity:activity-compose:1.9.0")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.7.0")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.7.0")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.0")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.8.0")
     implementation("androidx.navigation:navigation-compose:2.7.7")
     implementation("androidx.compose.runtime:runtime-livedata")
     implementation("androidx.compose.runtime:runtime-rxjava2")
